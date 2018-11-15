05eb2a9395a2839629c11804a4fd6dd395e3c5c2
AbubakrAliFOX <44189367+AbubakrAliFOX@users.noreply.github.com>
2018-11-15 08:00:26 +0200

    Add files via upload

---
+++ js/jscontact.js
@@ -0,0 +1,154 @@
+function bw () {
+	document.getElementById('icon').src='../img/image.jpg';
+}
+
+
+function nrml () {
+	document.getElementById('icon').src='../img/ICON.jpg';
+}
+
+
+function nighty () {
+	document.querySelector('.toggle1').style.backgroundColor='#1B1B1B';
+	document.querySelector('.toggle2').style.backgroundColor='#1B1B1B';
+
+	document.querySelector('#header-content').style.color='white';
+	document.querySelector('#Designerbtn').style.color='white';
+	document.querySelector('.button').style.backgroundColor='#36454F';
+
+	document.querySelector('.quote').style.color='white';
+	document.querySelector('#r1').style.color='white';
+	document.querySelector('#r2').style.color='white';
+	document.querySelector('#r3').style.color='white';
+
+	document.querySelector('#sec1').style.color='white';
+	document.querySelector('#num').style.color='white';
+
+	// for the backgrounds
+	document.querySelector('.cont1').style.backgroundColor='	#1B1B1B';
+	document.querySelector('.cont2').style.backgroundColor='	#1B1B1B';
+	document.querySelector('.cont3').style.backgroundColor='	#1B1B1B';
+	document.querySelector('.cont4').style.backgroundColor='	#1B1B1B';
+
+	document.querySelector('#body').style.backgroundColor='#1B1B1B';
+
+	document.getElementById('img1').src = '../img/socials/iconmonstr-facebook-3-240d.png';
+	document.getElementById('img2').src = '../img/socials/iconmonstr-instagram-11-240d.png';
+	document.getElementById('img3').src = '../img/socials/iconmonstr-linkedin-3-240d.png';
+	document.getElementById('img4').src = '../img/socials/iconmonstr-twitter-3-240d.png';
+	document.getElementById('img5').src = '../img/socials/iconmonstr-youtube-8-240d.png';
+	document.getElementById('img6').src = '../img/socials/iconmonstr-behance-3-240d.png';
+
+
+
+
+	document.querySelector('#foot1').style.color='white';
+
+
+
+
+
+
+
+
+
+	document.querySelector('.caption-1').style.color='white';
+	document.querySelector('.caption-2').style.color='white';
+	document.querySelector('.caption-3').style.color='white';
+	document.querySelector('.caption-4').style.color='white';
+	// document.querySelector('.caption-5').style.color='white';
+	// document.querySelector('.caption-6').style.color='white';
+	// document.querySelector('.caption-7').style.color='white';
+	// document.querySelector('.caption-8').style.color='white';
+	// document.querySelector('.caption-9').style.color='white';
+
+	
+	document.querySelector('.details-1').style.color='white';
+	document.querySelector('.details-2').style.color='white';
+	document.querySelector('.details-3').style.color='white';
+	document.querySelector('.details-4').style.color='white';
+	// document.querySelector('.details-5').style.color='white';
+	// document.querySelector('.details-6').style.color='white';
+	// document.querySelector('.details-7').style.color='white';
+	// document.querySelector('.details-8').style.color='white';
+	// document.querySelector('.details-9').style.color='white';
+	document.querySelector('.details-2s').style.color='white';
+	document.querySelector('.details-4s').style.color='white';
+
+
+		
+
+
+	
+
+}
+
+
+function day () {
+	document.querySelector('.toggle1').style.backgroundColor='#fdf5e6';
+	document.querySelector('.toggle2').style.backgroundColor='#fdf5e6';
+	
+	document.querySelector('#header-content').style.color='black';
+	document.querySelector('#Designerbtn').style.color='black';
+	document.querySelector('.button').style.backgroundColor='#FFD700';
+
+	document.querySelector('.quote').style.color='black';
+	document.querySelector('#r1').style.color='black';
+	document.querySelector('#r2').style.color='black';
+	document.querySelector('#r3').style.color='black';
+
+	document.querySelector('#sec1').style.color='black';
+	document.querySelector('#num').style.color='black';
+
+	// for the backgrounds
+	document.querySelector('.cont1').style.backgroundColor='	#fdf5e6';
+	document.querySelector('.cont2').style.backgroundColor='	#fdf5e6';
+	document.querySelector('.cont3').style.backgroundColor='	#fdf5e6';
+	document.querySelector('.cont4').style.backgroundColor='	#fdf5e6';
+
+	document.querySelector('#body').style.backgroundColor='#FFF8DC';
+
+
+
+
+
+	document.querySelector('#foot1').style.color='black';
+
+
+
+
+
+
+
+
+
+	document.querySelector('.caption-1').style.color='black';
+	document.querySelector('.caption-2').style.color='black';
+	document.querySelector('.caption-3').style.color='black';
+	document.querySelector('.caption-4').style.color='black';
+	// document.querySelector('.caption-5').style.color='black';
+	// document.querySelector('.caption-6').style.color='black';
+	// document.querySelector('.caption-7').style.color='black';
+	// document.querySelector('.caption-8').style.color='black';
+	// document.querySelector('.caption-9').style.color='black';
+
+	
+	document.querySelector('.details-1').style.color='black';
+	document.querySelector('.details-2').style.color='black';
+	document.querySelector('.details-3').style.color='black';
+	document.querySelector('.details-4').style.color='black';
+	// document.querySelector('.details-5').style.color='black';
+	// document.querySelector('.details-6').style.color='black';
+	// document.querySelector('.details-7').style.color='black';
+	// document.querySelector('.details-8').style.color='black';
+	// document.querySelector('.details-9').style.color='black';
+	document.querySelector('.details-2s').style.color='black';
+	document.querySelector('.details-4s').style.color='black';
+
+
+		
+
+
+	
+
+}
